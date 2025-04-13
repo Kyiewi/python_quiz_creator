3b22b5b3f6d6af90d7b86c5984e6b1bbe9746f8d
Kyiewi <kyrie.norriega@gmail.com>
2025-04-13 22:01:17 +0800

    Initializing pygame and mixer and setting up display and caption

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,14 @@
 #Import necessary modules
 import sys
 import pygame
-import pygame.locals import *
+import pygame.locals import
+from pyrect import WIDTH *
 
+#initialize pygame and mixer(for sound)
+pygame.init()
+pygame.mixer.init()
+
+#Setup Display
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Quiz Creator")
