58d6fda0a42bc5fbf0dc5bee7f3921c01279a1a8
Kyiewi <kyrie.norriega@gmail.com>
2025-05-08 16:56:01 +0800

    Update asset directory and add new resources

--- interactive_quiz_creator_game.py
+++ interactive_quiz_creator_game.py
@@ -25,21 +25,21 @@ BLACK = (0, 0, 0)
 def load_and_scale(path):
     return pygame.transform.scale(pygame.image.load(path), (WIDTH, HEIGHT))
 
-#-------ASSETS--------
+# Assets
+quiz_template   = load_and_scale('ASSET 2/quiz_template.png')
+sad_image       = load_and_scale('ASSET/sad.png')
+exit_image      = load_and_scale('ASSET/Exit.png')
+correct_image   = load_and_scale('ASSET/correct.png')
+wrong_image     = load_and_scale('ASSET/wrong.png')
+start_images    = [load_and_scale(f'ASSET 2/START/Start ({num}).png') for num in range(1, 13)]
+loading_images  = [load_and_scale(f'ASSET/LOADING/loading ({num}).png') for num in range(1, 23)]
 
-#Load Background images
-quiz_template = load_and_scale('ASSET/quiz_template.png')
-sad_image = load_and_scale('ASSET/sad.png')
-exit_image = load_and_scale('ASSET/Exit.png')
-
-#Load animations
-start_images = [load_and_scale(f'ASSET/START/start ({num}).png')for num in range(1, 13)]
-loading_images = [load_and_scale(f'ASSET/LOADING/loading ({num}).png')for num in range(1, 23)]
-
-#Load sounds
 click_sound = pygame.mixer.Sound('SOUNDS/click.mp3')
 pygame.mixer.music.load('SOUNDS/background music.mp3')
-pygame.mixer.music.play(-1) #loop background music
+pygame.mixer.music.play(-1)
+
+# Event for auto-advance after result
+RESULT_TIMER = USEREVENT + 2
 
 #---------------INPUTBOX CLASS--------------
 
